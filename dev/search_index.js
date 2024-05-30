894ff155ad19998ad61c2ca1bb9a9cbb13c9244e
Documenter.jl <documenter@juliadocs.github.io>
2024-05-30 07:23:49 +0000

    build based on 35c4ec1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TamakiTreeWidth","category":"page"},{"location":"#TamakiTreeWidth","page":"Home","title":"TamakiTreeWidth","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TamakiTreeWidth.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TamakiTreeWidth]","category":"page"}]
+}
