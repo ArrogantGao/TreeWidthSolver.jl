01e7ed1787588b3f1953e2e5ccea8d7d66050901
GiggleLiu <cacate0129@gmail.com>
2025-05-13 01:59:41 +0800

    remove piracy to BitBasis

--- test/bitbasis.jl
+++ test/bitbasis.jl
@@ -7,11 +7,3 @@ using TreeWidthSolver: bit2id
     b = bmask(LongLongUInt{2}, 1:65)
     @test bit2id(b, 128) == collect(1:65)
 end
-
-@testset "LongLongUInt hash" begin
-    b1 = bmask(LongLongUInt{1}, 1)
-    b2 = bmask(LongLongUInt{1}, 2)
-    b3 = bmask(LongLongUInt{1}, 3)
-
-    @test hash((b1, b2, b3)) == hash((b1, b2, b3))
-end
